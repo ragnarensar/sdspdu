9899c498e5fe3293b92cabf81cff40540d27fa0a
ragnarensar <ragnarensar@vodafone.de>
2025-04-13 21:08:52 +0200

    extended to output either JSON or string as an array for import in Bash

--- SDSdecode.php
+++ SDSdecode.php
@@ -3,17 +3,44 @@
 
 require_once('TetraSDS/PDU.decode.php');
 
-foreach(decode_PDU($argv[1]) as $key => $value)
+$longopts  = array(                     
+    "pdu:",             // Required value PDU of a Tetra Transport Layer SDS
+    "output:",         // output format: json (--output json)
+);                                                                                   
+                                                                                     
+$args=array();                                                                       
+                                                                                     
+foreach(getopt("", $longopts) as $key => $value) $args[$key] = $value;
+
+if (!(array_key_exists('pdu', $args))) {
+
+    fwrite(STDERR, "missing required option --pdu\n");
+    exit(1);
+
+}
+
+$decoded = decode_PDU($args['pdu']);
+
+if (array_key_exists('output', $args) && $args['output'] == 'json') {
+
+    $JSON_PDU = json_encode($decoded,JSON_UNESCAPED_UNICODE);
+    echo $JSON_PDU."\n";
+
+}
+else
 {
-	if (!(is_array($value))) {
-		echo "PDUelements[\"".$key."\"]=\"".$value."\"\n";
-	}
-	else
-	{
-		foreach($value as $key2 => $value2)
-		{
-			 echo "PDUelements[\"".$key."_".$key2."\"]=\"".$value2."\"\n";
-		}
-	}
+    foreach($decoded as $key => $value)
+    {
+        if (!(is_array($value))) {
+            echo "PDUelements[\"".$key."\"]=\"".$value."\"\n";
+        }
+        else
+        {
+            foreach($value as $key2 => $value2)
+            {
+                echo "PDUelements[\"".$key."_".$key2."\"]=\"".$value2."\"\n";
+            }
+        }
+    }
 }
 ?>
