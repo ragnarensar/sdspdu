47ea5ca009eac9b44296c4166cdd1a919439c18f
ragnarensar <ragnarensar@vodafone.de>
2025-04-23 21:11:19 +0200

    Default

--- SDSdecode.php
+++ SDSdecode.php
@@ -12,26 +12,24 @@ $args=array();
                                                                                      
 foreach(getopt("", $longopts) as $key => $value) $args[$key] = $value;
 
-if (!(array_key_exists('pdu', $args))) {
-
+if (!(array_key_exists('pdu', $args))) 
+{
     fwrite(STDERR, "missing required option --pdu\n");
     exit(1);
-
 }
 
 $decoded = decode_PDU($args['pdu']);
 
-if (array_key_exists('output', $args) && $args['output'] == 'json') {
-
-    $JSON_PDU = json_encode($decoded,JSON_UNESCAPED_UNICODE);
-    echo $JSON_PDU."\n";
-
+if (array_key_exists('output', $args) && $args['output'] == 'json') 
+{
+    echo json_encode($decoded,JSON_UNESCAPED_UNICODE)."\n";
 }
 else
 {
     foreach($decoded as $key => $value)
     {
-        if (!(is_array($value))) {
+        if (!(is_array($value))) 
+        {
             echo "PDUelements[\"".$key."\"]=\"".$value."\"\n";
         }
         else
