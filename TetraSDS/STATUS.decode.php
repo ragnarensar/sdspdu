3ef9ff3f2c718a1b44db921aa64edbc0e0a35e3e
ragnarensar <ragnarensar@vodafone.de>
2025-07-13 16:12:06 +0200

    ad-hoc

--- TetraSDS/STATUS.decode.php
+++ TetraSDS/STATUS.decode.php
@@ -4,9 +4,7 @@ function decode_STATUS ($STATUS)
     $yml_content = yaml_parse_file('TetraSDS/STATUS.declaration.yml');
     print_r($yml_content);
     $key = array_search($STATUS, array_column($yml_content['tetra'], 'status'));
-    print_r($key);                                                 
-    print_r($yml_contnet['tetra'][$key]['declaration']);
-    $ret = $yml_contnet['tetra'][$key];
+    $ret = $yml_content['tetra'][$key];
     return $ret;
 }
 ?>
